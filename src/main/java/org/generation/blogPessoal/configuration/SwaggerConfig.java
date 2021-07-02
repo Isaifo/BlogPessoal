6ae0877f02ba762ce69932eef177516ccc90bc79
Isaifo <ysoca98@hotmail.com>
2021-07-02 19:05:37 -0300

    Adicionando o heroku

--- src/main/java/org/generation/blogPessoal/configuration/SwaggerConfig.java
+++ src/main/java/org/generation/blogPessoal/configuration/SwaggerConfig.java
@@ -23,7 +23,8 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 		.apis(RequestHandlerSelectors.basePackage("org.generation.blogPessoal.controller"))
-		.paths(PathSelectors.any()).build().apiInfo(metadata()).useDefaultResponseMessages(false)
+		.paths(PathSelectors.any()).build().apiInfo(metadata())
+		.useDefaultResponseMessages(false)
 		.globalResponses(HttpMethod.GET,responseMessageForGET ());
 	}
 
